ed700a79d2c5e8f4c7ee20ed20cda30c796a522a
kpko <plastocyst@googlemail.com>
2014-08-22 17:38:14 +0200

    1. Datenbankverbindung mit DbContext

---
+++ MyApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DbContext ctx = new DbContext("...");
+            var contacts = ctx.Database.SqlQuery<Contact>("select * from dbo.Contacts").ToList();
+        }
+    }
+
+    class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
