b4bc4d373a43596bea82a452f9b14e439e25036a
kpko <plastocyst@googlemail.com>
2014-08-22 18:17:28 +0200

    2. Hinzufügen eines neuen Datensatzes mit eigener DbContext-Klasse

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -11,14 +13,36 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            DbContext ctx = new DbContext("...");
-            var contacts = ctx.Database.SqlQuery<Contact>("select * from dbo.Contacts").ToList();
+            ContactsDB ctx = new ContactsDB();
+            ctx.Database.Log = (s) =>
+            {
+                Console.WriteLine(s);
+            };
+
+            var contacts = ctx.Contacts.ToList();
+
+
+            Contact myContact = new Contact();
+            myContact.FirstName = "Daisy";
+            myContact.LastName = "Duck";
+
+            ctx.Contacts.Add(myContact);
+            ctx.SaveChanges();
+
+            contacts = ctx.Contacts.ToList();
+
         }
     }
 
     class Contact
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+    }
+
+    class ContactsDB : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; }
     }
 }
